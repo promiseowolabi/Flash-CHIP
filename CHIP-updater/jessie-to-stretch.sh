1ad0eae28ce3d477c9aecda3e4ae904ef7ba4a9d
bicubico <nunez.alejandro@gmail.com>
2023-02-17 23:51:27 -0300

    echo

--- CHIP-updater/jessie-to-stretch.sh
+++ CHIP-updater/jessie-to-stretch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Upgrade jessie
+echo "."
+echo "*** Updating and upgrading Debian Jessie. ***"
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo touch /etc/apt/sources.list
 sudo chmod 666 /etc/apt/sources.list
@@ -11,11 +13,17 @@ echo -e "deb-src [check-valid-until=no] http://archive.debian.org/ jessie/update
 echo -e "deb http://chip.jfpossibilities.com/chip/debian/repo jessie main" >> /etc/apt/sources.list
 echo -e "deb http://chip.jfpossibilities.com/chip/debian/pocketchip jessie main" >> /etc/apt/sources.list
 
+echo "."
+echo "*** Installing debian keyring. ***"
 sudo apt install debian-keyring debian-archive-keyring -y --force-yes
 
+echo "."
+echo "*** apt update & upgrade ***"
 sudo apt update && sudo apt upgrade -y
 
 # Upgrade from jessie to stretch 
+echo "."
+echo "*** Upgrading Debian Jessie to Debian Stretch ***"
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak2
 sudo touch /etc/apt/sources.list
 sudo chmod 666 /etc/apt/sources.list
@@ -25,7 +33,9 @@ echo -e "deb-src http://ftp.us.debian.org/debian/ stretch main contrib non-free"
 echo -e "deb http://security.debian.org/ stretch/updates main contrib non-free" >> /etc/apt/sources.list
 echo -e "deb-src http://security.debian.org/ stretch/updates main contrib non-free" >> /etc/apt/sources.list
 
-sudo apt update && sudo apt upgrade -y --force-yes
+echo "."
+echo "*** apt update & upgrade ***"
+sudo apt update && sudo apt upgrade -y
 
 sudo chmod 666 /etc/NetworkManager/NetworkManager.conf
 echo -e "[connection]" >> /etc/NetworkManager/NetworkManager.conf
@@ -33,7 +43,5 @@ echo -e "wifi.mac-address-randomization=1" >> /etc/NetworkManager/NetworkManager
 echo -e "" >> /etc/NetworkManager/NetworkManager.conf
 echo -e "[device]" >> /etc/NetworkManager/NetworkManager.conf
 echo -e "wifi.scan-rand-mac-address=no" >> /etc/NetworkManager/NetworkManager.conf
-
-sudo apt autoremove
 
 echo "*** Update to Stretch finished. Reboot to continue the process. ***"
