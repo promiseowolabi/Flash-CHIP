2407801e7e3ab7697a409bf4d94eb7471f4d5cb3
bicubico <nunez.alejandro@gmail.com>
2023-02-21 17:42:11 -0300

    startup folder fix

--- CHIP-updater/install_extras.sh
+++ CHIP-updater/install_extras.sh
@@ -8,22 +8,26 @@ apt install neofetch -y
 echo "neofetch" >> /home/chip/.bashrc
 
 wget https://raw.githubusercontent.com/asophila/headless/main/startup.sh
-chmod +x startup.sh
+mv startup.sh /home/chip/
+chmod +x /home/chip/startup.sh
 
-sed -e -i '$i \sh /home/chip/startup.sh\n' /etc/rc.local
+sed -i '$i \sh /home/chip/startup.sh\n' /etc/rc.local
+chmod +x /etc/rc.local
 
 echo -n "Insert a name for the ntfy.sh group where the network IP will be posted:"
 read group
-if [$group==""]; then
-    $group=="secret_ip"
+if [[ -z "$group" ]]; then
+    $group="secret_ip"
 fi
 sed -i 's/SECRET_GROUP/$group/g' startup.sh
 
 echo -n "Insert a name for this host:"
 read hname
+if [[ -z "$hname" ]]; then
+    $hname="chip"
+fi
 
 hostnamectl set-hostname $hname
-
 
 echo "."
 echo "*** Done ***"
