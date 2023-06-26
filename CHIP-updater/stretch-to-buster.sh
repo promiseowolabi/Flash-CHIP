a9b94f6d9dbd601aa97192e50fbea8748fb3701e
Promise Owolabi <promise.owolabi@me.com>
2023-06-26 22:05:17 +0100

    Update stretch-to-buster.sh

--- CHIP-updater/stretch-to-buster.sh
+++ CHIP-updater/stretch-to-buster.sh
@@ -4,7 +4,7 @@
 echo "."
 echo "*** Updating and upgrading Debian Stretch to Buster. ***"
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
-wget https://raw.githubusercontent.com/asophila/Flash-CHIP/master/CHIP-updater/buster_source_list.txt
+wget https://raw.githubusercontent.com/promiseowolabi/Flash-CHIP/master/CHIP-updater/buster_source_list.txt
 mv buster_source_list.txt /etc/apt/sources.list
 
 echo "."
@@ -13,12 +13,6 @@ apt update
 apt install linux-image-armmp -y --force-yes
 apt full-upgrade -y --force-yes
 sleep 5
-
-
-# Define X11 variables for buster
-sudo mv /etc/X11/xorg.conf /etc/X11/xorg.conf.bak
-wget https://raw.githubusercontent.com/asophila/Flash-CHIP/master/CHIP-updater/buster_x11.txt
-mv buster_x11.txt /etc/X11/xorg.conf
 
 apt autoremove -y --force-yes
 echo "."
